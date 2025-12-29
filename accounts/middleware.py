d6cafb694840c7c1703139ba85f723c46cd1d40c
VLS <kabina122@gmail.com>
2025-12-30 00:00:51 +0300

    Fix middleware syntax error

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,10 +1,22 @@
-# accounts/middleware.py
 """
 Простые middleware для отладки
 """
 
 class SimpleMiddleware:
     """Простой middleware для тестирования"""
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        # Код до обработки запроса
+        response = self.get_response(request)
+        # Код после обработки запроса
+        return response
+
+# ↓↓↓ ОТДЕЛЬНЫЙ класс (не внутри SimpleMiddleware)
+class CustomMiddleware:
+    """Кастомный middleware"""
     
     def __init__(self, get_response):
         self.get_response = get_response
