32a79e173533b7af5ea8b676c036ac0816c18404
VLS <kabina122@gmail.com>
2025-12-31 15:21:58 +0300

    фикс кнопки тест добавления2

--- lk_clone/urls.py
+++ lk_clone/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('cart/update/<int:item_id>/', views.update_cart_item_view, name='update_cart_item'),
     path('cart/clear/', views.clear_cart_view, name='clear_cart'),
     path('cart/checkout/', views.checkout_from_cart, name='checkout_from_cart'),
-     path('cart/get-count/', views.get_cart_count, name='get_cart_count'),
+    path('cart/get-count/', views.get_cart_count, name='get_cart_count'),
     path('test-simple-add/<int:product_id>/', views.test_simple_add, name='test_simple_add'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
